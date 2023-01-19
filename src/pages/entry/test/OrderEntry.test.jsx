d972da9bf960e8c8dbb7bdd0b753567ccd3a89df
CarlosDahmen <carlos.dahmen@gmail.com>
2023-01-19 13:38:46 -0500

    Test and code context for scoop subtotal

--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -1,9 +1,14 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+} from "../../../test-utils/testing-library-utils";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/servers";
+// import { OrderDetailsProvider } from "../../../context/OrderDetails";
 
-test("handles eroors for scoops and topping routes", async () => {
+test("handles errors for scoops and topping routes", async () => {
   server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
